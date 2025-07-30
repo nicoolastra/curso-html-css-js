e9385685db76d7a4aea111ab1f70c281d8c268e2
nicoolastra <nicoolastra@gmail.com>
2025-07-29 22:47:11 -0300

    actualizacion al dia de la fecha

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-const seccion = document.getElementById('seccion1');
+console.log('scrollY:', window.scrollY);
 
-seccion.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => {
+  
   const nav = document.getElementById('navegador');
-  console.log("scrollTop:", seccion.scrollTop);
-
-  if (seccion.scrollTop > 50) {
-    nav.style.backgroundColor = '#fff';
-    nav.style.boxShadow = '0 2px 10px rgba(0,0,0,0.1)';
+  console.log('scrollY:', window.scrollY);
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
   } else {
-    nav.style.backgroundColor = 'transparent';
-    nav.style.boxShadow = 'none';
+    nav.classList.remove('scrolled');
   }
 });
+
+window.addEventListener('scroll', () => { // Para verificar el scroll de la ventana
+  console.log('window scrollY:', window.scrollY); // Muestra la posición del scroll de la ventana
+});
+
+document.getElementById('seccion1').addEventListener('scroll', () => { // Para verificar el scroll de un elemento específico
+  console.log('seccion1 scrollTop:', document.getElementById('seccion1').scrollTop); // Muestra la posición del scroll del elemento con id "seccion1"
+});
+
+
+
+
+/* para que la animacion de la imagen se reproduzca una vez que el usuario hace scroll */
+
+
+const observer = new IntersectionObserver((entries) => { // Crea un nuevo IntersectionObserver
+  entries.forEach(entry => { // Recorre cada entrada del observer
+    if (entry.isIntersecting) { // Verifica si la entrada está intersectando
+      entry.target.classList.add('aparecer-izq'); // Agrega la clase 'aparecer' al elemento que está intersectando
+    }
+  });
+}, {
+  threshold: 0.5 // ajustá esto según el punto donde querés que se active
+});
+

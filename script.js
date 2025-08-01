66c61748113be87fc012ffa9b0af864a35f40813
nicoolastra <nicoolastra@gmail.com>
2025-07-31 22:35:26 -0300

    actualizacion del dia

--- script.js
+++ script.js
@@ -10,28 +10,117 @@ window.addEventListener('scroll', () => {
     nav.classList.remove('scrolled');
   }
 });
-
 window.addEventListener('scroll', () => { // Para verificar el scroll de la ventana
   console.log('window scrollY:', window.scrollY); // Muestra la posición del scroll de la ventana
 });
 
-document.getElementById('seccion1').addEventListener('scroll', () => { // Para verificar el scroll de un elemento específico
-  console.log('seccion1 scrollTop:', document.getElementById('seccion1').scrollTop); // Muestra la posición del scroll del elemento con id "seccion1"
-});
 
-
-
-
+// PAGINA 2: ANIMACIONES DE LAS TARJETAS
 /* para que la animacion de la imagen se reproduzca una vez que el usuario hace scroll */
+let primerScroll = true;
 
+window.addEventListener("scroll", () => {
+  if (primerScroll) {
+    document.querySelector(".tarjeta-historia1").classList.add("visible");
+    primerScroll = false;
+  }
+});
+const tarjetas = document.querySelectorAll('#contenedorTarjetas > div');
 
-const observer = new IntersectionObserver((entries) => { // Crea un nuevo IntersectionObserver
-  entries.forEach(entry => { // Recorre cada entrada del observer
-    if (entry.isIntersecting) { // Verifica si la entrada está intersectando
-      entry.target.classList.add('aparecer-izq'); // Agrega la clase 'aparecer' al elemento que está intersectando
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    console.log('Intersección detectada:', entry.target);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
     }
   });
 }, {
-  threshold: 0.5 // ajustá esto según el punto donde querés que se active
+  threshold: 1
 });
 
+
+tarjetas.forEach(tarjeta => observer.observe(tarjeta));
+
+// PAGINA 2: BOTONES DEL VIDEO
+
+const video = document.getElementById('video');
+const botonplay = document.getElementById('play');
+const botonpausa = document.getElementById('pausa');
+botonplay.innerHTML = '<img src=./multimedia/imagenes/play.svg alt="play">';
+botonpausa.innerHTML = '<img src=./multimedia/imagenes/pausa.svg alt="pause">';
+botonplay.addEventListener('click', () => {
+  video.play();
+  
+});
+botonpausa.addEventListener('click', () => {
+  video.pause();
+  
+});
+// Actualizar el tiempo actual del video cada segundo
+setInterval(() => {
+  const tiempoActual = document.getElementById('tiempoActual');
+  const minutos = Math.floor(video.currentTime / 60);
+  const segundos = Math.floor(video.currentTime % 60);
+  tiempoActual.textContent = `${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
+}, 1000);
+
+
+// PAGINA 3: ROMPECABEZAS
+
+ document.querySelectorAll('img[id^="imagen"]').forEach(img => {
+    img.setAttribute('draggable', 'true');
+    img.addEventListener('dragstart', evento => {
+      evento.dataTransfer.setData('idImagen', img.id);
+    });
+  });
+
+  let piezasCorrectas = 0;
+  let piezasColocadas = 0;
+  // Seleccionamos todas las zonas de soltar
+  document.querySelectorAll('.zona-soltar').forEach((zona, i) => {
+  zona.addEventListener('dragover', evento => evento.preventDefault());
+
+  zona.addEventListener('drop', evento => {
+    const idImagen = evento.dataTransfer.getData('idImagen');
+    const imagen = document.getElementById(idImagen);
+    const zonaID = `zona${i + 1}`;
+    const imagenID = `imagen${i + 1}`;
+
+    if (!zona.querySelector('img')) {
+      zona.innerHTML = '';
+      zona.appendChild(imagen);
+      imagen.classList.add('sin-rotacion');
+      piezasColocadas++;
+
+      if (zona.id === zonaID && idImagen === imagenID) {
+        piezasCorrectas++;
+      }
+
+      if (piezasColocadas === 3) {
+        const contenedor = document.querySelector('.contenedorCajas');
+        contenedor.style.gap = '0px';
+        contenedor.style.transition = 'transform 0.8s ease, opacity 0.8s ease';
+        contenedor.style.transform = 'scale(1.3)';
+        const boton = document.querySelector('.reiniciar');
+        boton.style.display = 'block';
+        setTimeout(() => {
+          contenedor.style.opacity = '0';
+        }, 900);
+
+        setTimeout(() => {
+          contenedor.style.display = 'none';
+          const mensajeFinal = document.getElementById('mensajeFinal');
+
+          if (piezasCorrectas === 3) {
+            mensajeFinal.innerHTML = '<h1 style="color: #2ecc71;">¡Felicitaciones! Completaste el rompecabezas 🎉</h1>';
+          } else {
+            mensajeFinal.innerHTML = '<h1 style="color: #e74c3c;">Lo sentimos, rompecabezas fallido 😔</h1>';
+          }
+
+          mensajeFinal.style.display = 'block';
+        }, 1700);
+      }
+    }
+  });
+});
